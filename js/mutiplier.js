0af00d146cbfac3ccb6271d8026a265beeac0dfb
Zian Pan <zpan@algomau.ca>
2025-02-06 14:49:33 -0500

    asdasda

--- js/mutiplier.js
+++ js/mutiplier.js
@@ -1,7 +1,5 @@
-function multiplier(times) {
-  function muliply(number) {
-    let answer = times * number;
-    return answer;
-  }
-  return muliply;
+function enter() {
+  let input = document.getElementById("numberInput").value;
+  let square = input * input;
+  document.getElementById("output").textContent = `Your input is ${square}`;
 }
